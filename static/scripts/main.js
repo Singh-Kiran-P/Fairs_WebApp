a3a3d946046664311972254ac1c4df1d65777a83
Singh Kiran <kiran.singh@student.uhasselt.be>
2020-10-14 15:11:56 +0200

    old top menu

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,6 +1,9 @@
-let mainNav = document.getElementById('js-menu');
-let navBarToggle = document.getElementById('js-navbar-toggle');
-
-navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
-});
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}

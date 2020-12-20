70b9bb55dfb4a18b0a2d8a2463abd7873e42fe05
Singh Kiran <kiran.singh@student.uhasselt.be>
2020-12-20 14:27:09 +0100

    addzone+addzoneSlot samen

--- static/dashboard/city/addZone.php
+++ static/dashboard/city/addZone.php
@@ -4,6 +4,8 @@ require '../../../server/classes/class.upload.php';
 session_start();
 $errorMsg = "";
 $tileFair = "";
+$enabled = "";
+$disabled = "disabled";
 if (isset($_GET['fairId']) && isset($_GET['title'])) {
   $_SESSION['fairId'] = $_GET['fairId'];
   $tileFair = $_GET['title'];
@@ -36,9 +38,11 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['type']) && $_SESSION['type'
       $i = Upload::uploadFiles($files, $zoneId, "zone", "img");
       $v = Upload::uploadFiles($video, $zoneId, "zone", "video");
       if (is_numeric($zoneId)) {
+        $_SESSION['zoneId'] =    $zoneId;
+
         $fair->updateDbFileCount($zoneId, $i, $v, "zones");
         $errorMsg .= 'Zone is added successfully';
-
+        $enabled = "disabled";
         //reset form
         $title = "";
         $open_spots = "";
@@ -58,7 +62,19 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['type']) && $_SESSION['type'
         $errorMsg .= '<br><br>Video error : ' . $errorMsgUploadVideo['msg'];
     }
   }
-  if (isset($_POST['submit_Save'])) {
+  if (isset($_POST['submit_slot'])) {
+    $openingSlot = $_POST['openingSlot'];
+    $closingSlot = $_POST['closingSlot'];
+
+    $Msg = $fair->checkingZoneSlot($_SESSION['zoneId'], $openingSlot, $closingSlot);
+    if ($Msg == '') {
+      $fairModel = $fair->getFairModel($_SESSION['fairId']);
+
+      $zones = $fair->getAllZones($_SESSION['fairId']);
+      foreach ($zones as $z) {
+        $Msg = $fair->addZoneSlot($z['zone_id'], $openingSlot, $closingSlot, $z['freeSlots'], $fairModel->getVar());
+      }
+    }
     header("Location: addZoneSlot.php?zoneId=" .  $_GET['zoneId'] . "&free_slots=" . $_GET['open_spots']);
   }
 } else {
@@ -89,23 +105,23 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['type']) && $_SESSION['type'
   </header>
 
   <!-- The flexible grid (content) -->
-  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>?zoneId=<?php if (isset($zoneId)) echo $zoneId ?>&open_spots=<?php if (isset($_POST['open_spots'])) echo $_POST['open_spots'] ?>&fairId=<?php if (isset($_SESSION['fairId'])) echo $_SESSION['fairId'] ?>" method="post" class="content" id="form" onsubmit="return validateForm()" enctype='multipart/form-data'>
+  <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post" class="content" id="form" onsubmit="return validateForm()" enctype='multipart/form-data'>
 
     <div class="mainCol1 g">
       <center>
 
         <h1 class="topTitle">Add Zone <?php echo $tileFair;  ?></h1>
 
-        <input type="text" name="title" placeholder="Title" value="<?php if (isset($title)) echo $title; ?>" required>
+        <input type="text" name="title" placeholder="Title" value="<?php if (isset($title)) echo $title; ?>" required <?php echo $enabled; ?>>
 
-        <textarea type="" name="desc" placeholder="Give a short discription about this fair" form="form" required><?php if (isset($desc)) echo $desc; ?></textarea>
-        <textarea type="" name="attractions" placeholder="Give the attaction on this Zone like ->  attraction1,attraction2 " form="form" required><?php if (isset($attractions)) echo $attractions; ?></textarea>
+        <textarea type="" name="desc" placeholder="Give a short discription about this fair" form="form" required <?php echo $enabled; ?>><?php if (isset($desc)) echo $desc; ?></textarea>
+        <textarea type="" name="attractions" placeholder="Give the attaction on this Zone like ->  attraction1,attraction2 " form="form" required <?php echo $enabled; ?>><?php if (isset($attractions)) echo $attractions; ?></textarea>
 
         <div>
           <div class="sidebyside">
-            <input type="number" min="1" name="open_spots" placeholder="Total free spots" value="<?php if (isset($open_spots)) echo $open_spots; ?>" required>
+            <input type="number" min="1" name="open_spots" placeholder="Total free spots" value="<?php if (isset($open_spots)) echo $open_spots; ?>" required <?php echo $enabled; ?>>
 
-            <input type="text" name="location" placeholder="Location" value="<?php if (isset($location)) echo $location; ?>" required>
+            <input type="text" name="location" placeholder="Location" value="<?php if (isset($location)) echo $location; ?>" required <?php echo $enabled; ?>>
           </div>
         </div>
 
@@ -113,13 +129,22 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['type']) && $_SESSION['type'
         Images: <h5> (only JPG, JPEG, PNG & GIF files are allowed Max 5mb )</h5>
 
 
-        <input type="file" name="file[]" class="inputfile" value="" multiple>
+        <input type="file" name="file[]" class="inputfile" value="" multiple <?php echo $enabled; ?>>
 
         Video's: <h6> (only MP4 and MK files are allowed Max 20mb )</h6>
 
-        <input type="file" name="video[]" class="inputfile" value="" multiple>
+        <input type="file" name="video[]" class="inputfile" value="" multiple <?php echo $enabled; ?>>
 
 
+        <!-- time slots -->
+        <div>
+          Time slots:
+          <div class="sidebyside">
+            <input type="text" name="openingSlot" placeholder="Opening Slot" onfocus="(this.type='time')" onblur="(this.type='text')" <?php echo $disabled; ?> >
+            <input type="text" name="closingSlot" placeholder="Closing Slot" onfocus="(this.type='time')" onblur="(this.type='text')" <?php echo $disabled; ?>>
+          </div>
+        </div>
+        <button type="button" name="submit_slot" id="btn">Add Slot</button>
 
         <p id="error">
           <?php
@@ -130,16 +155,14 @@ if (isset($_SESSION['loggedin']) && isset($_SESSION['type']) && $_SESSION['type'
         </p>
 
       </center>
-      <button type="submit" name="submit" id="btn">Add</button>
+      <?php if ($enabled == "") echo '<button type="submit" name="submit" id="btn">Add Zone</button>'; ?>
+
 
 
     </div>
 
-    <?php echo 'addZoneSlot.php?zoneId="' .  $_GET["zoneId"] . '"&free_slots="' . $_GET["open_spots"]; ?>
-    <button type="button" onclick="location.href=''">Next</button>
 
 
-    <!-- <button type="button" name="submit_Save" id="btn"><a href="http://">Next</a> Next</button> -->
 
   </form>
 
